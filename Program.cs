b821a52ae0313e00799b51a6fc5c29fd7ab5fa5f
dk.raw <hello@dkatsios.tk>
2021-03-06 12:34:23 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inheritance2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Electrician e1 = new Electrician();
+            e1.Print();
+            Console.WriteLine("=========================================");
+            Electrician e2 = new Electrician("David Jones", 17000);
+            e2.Print();
+            Console.WriteLine("=========================================");
+            Electrician e3 = new Electrician("Maria Doe", 30000, null);
+            e3.Print();
+
+        }
+    }
+    class Worker
+    {
+        public string Name { get; set; }
+        public int Salary { get; set; }
+        public Worker()
+        {
+            
+        }
+        public Worker(int mistos)
+        {
+            Salary = mistos * 50 / 100;
+        }
+        public void Print()
+        {
+            Console.WriteLine($"{Name, -15} {Salary, -10}");
+        }
+
+    }
+    class Electrician : Worker
+    {
+        public Electrician() :base()
+        {
+            Name = "Unknown";
+            Salary = 0;
+        }
+        public Electrician(string name, int salary) :base()
+        {
+            Salary = salary;
+            Name = name;
+        }
+        public Electrician(string name, int salary, string smthng) :base(salary)
+        {
+            Name = name;
+        }
+    }
+
+}
